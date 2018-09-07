75448f4b85cc38d57dfc20fadd881a3eab7a279b
Faith <pfaithmtan@gmail.com>
2018-09-06 17:32:07 -0700

    Start seeds

--- PollsApp/db/seeds.rb
+++ PollsApp/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+drake = User.create!(username: 'Drake')
+johnny_pancakes = User.create!(username: 'Johnny Pancakes')
+stevie_wonder = User.create!(username: 'Stevie Wonder')
+beyonce = User.create!(username: 'Beyonce')
+kanye = User.create!(username: 'Kanye')
+
+poll1 = Poll.create!(author_id: User.all.first.id, title: 'My dance moves')
